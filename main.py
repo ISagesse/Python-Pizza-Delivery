b2e845fb93efc19386e2e5282d70479b0cfa9a84
Israel Sagesse <israelsagesse@gmail.com>
2021-04-22 15:01:20 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+# 🚨 Don't change the code below 👇
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L ")
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+bill = 0
+
+if size == "s":
+  bill = 15
+
+if size == "m":
+  bill = 20
+
+if size == "l" :
+  bill = 25
+
+if add_pepperoni == "y" and size == "s":
+  bill += 2
+else:
+  bill += 3
+
+if extra_cheese == "y":
+  bill += 1
+
+print(f"Your total bill for the pizza is ${bill}")
+
+
+
+
